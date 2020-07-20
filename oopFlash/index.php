0b9af0e129f01d6328e6a72eaf2a194b2fa56c31
Adine07 <adinepamungkas07@gmail.com>
2020-07-20 20:54:31 +0700

    3rd Commit

--- oopFlash/index.php
+++ oopFlash/index.php
@@ -7,10 +7,16 @@
 	{
 
 		protected $halaman;
+		public $luasHalaman = 10;
 
 		public function ShowHalaman(){
 			return $this->halaman;
 		}
+
+		public function SetLuasHalaman($luasHalaman){
+			$this->luasHalaman = $luasHalaman * 2;
+		}
+
 	}
 
 	
@@ -28,6 +34,10 @@
 
 		public function SetLantai($lantai){
 			$this->lantai = $lantai;
+		}
+
+		public function SetLuasHalaman($luasHalaman){
+			$this->luasHalaman = $luasHalaman;
 		}
 
 		public function ShowType(){
@@ -50,14 +60,16 @@
 	// echo $rumah->ShowHalaman();
 
 	$rumah = new Rumah("2x1", "taman bunga");
+	$rumah->SetLuasHalaman(21);
 
-	echo "Saya memesan rumah dengan type: " . $rumah->ShowType() . " dan memiliki " . $rumah->ShowLantai() . " lantai, dengan halaman " . $rumah->ShowHalaman();
+	echo "Saya memesan rumah dengan type: " . $rumah->ShowType() . " dan memiliki " . $rumah->ShowLantai() . " lantai, dengan halaman " . $rumah->ShowHalaman() . " seluas " . $rumah->luasHalaman . "km.";
 
 	echo "<br/><br/>";
 
 	$rumah->SetLantai("tujuh");
+	$rumah->SetLuasHalaman(12);
 
-	echo "Saya memesan rumah dengan type: " . $rumah->ShowType() . " dan memiliki " . $rumah->ShowLantai() . " lantai, dengan halaman " . $rumah->ShowHalaman();
+	echo "Saya memesan rumah dengan type: " . $rumah->ShowType() . " dan memiliki " . $rumah->ShowLantai() . " lantai, dengan halaman " . $rumah->ShowHalaman() . " seluas " . $rumah->luasHalaman . "km.";
 
 
 ?>
