2d4190020841479882a7f7a8d79b46b0acc498c6
Adine07 <adinepamungkas07@gmail.com>
2020-07-22 20:52:01 +0700

    Laravel latihan 1

---
+++ latihan/resources/views/guardian/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Guardians | School</title>
+</head>
+
+<body>
+
+
+	<h1>Data Wali</h1>
+
+	<table border="1" style="width: 100%;">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>NIK</th>
+				<th>gender</th>
+				<th>Phone</th>
+				<th>Tanggal Lahir</th>
+				<th>Alamat</th>
+				<th>Kandung</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			@foreach($guardians as $guardian)
+			<tr>
+				<td>{{ $guardian->name }}</td>
+				<td>{{ $guardian->nik }}</td>
+				<td>{{ ($guardian->gender == 'l') ? 'laki-laki' : 'perempuan' }}</td>
+				<td>{{ $guardian->phone }}</td>
+				<td>{{ $guardian->birth_date }}</td>
+				<td>{{ $guardian->address }}</td>
+				<td>{{ $guardian->is_parent ? 'Ya' : 'Tidak' }}</td>
+			</tr>
+			@endforeach
+		</tbody>
+
+	</table>
+
+</body>
+
+</html>
---
+++ tugas/resources/views/article/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Article | MyBlog</title>
+</head>
+
+<body>
+
+
+	<h1>List Article</h1>
+
+	<table border="1" style="width: 100%;">
+		<thead>
+			<tr>
+				<th>Title</th>
+				<th>Author</th>
+				<th>Content</th>
+				<th>Category</th>
+				<th>Created_at</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			@foreach($articles as $article)
+			<tr>
+				<td>{{ $article->title }}</td>
+				<td>{{ $article->author }}</td>
+				<td>{{ $article->content }}</td>
+				<td>{{ $article->category }}</td>
+				<td>{{ $article->created_at }}</td>
+			</tr>
+			@endforeach
+		</tbody>
+
+	</table>
+
+</body>
+
+</html>
